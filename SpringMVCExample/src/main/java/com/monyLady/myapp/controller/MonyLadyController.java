eaefe6c972d4a7ae6df19991bea09891d09e1f44
aouldch <aboubecrine@gmail.com>
2015-03-06 20:24:17 +0100

    Définition de quelques services métiers concernant les produits

--- SpringMVCExample/src/main/java/com/monyLady/myapp/controller/MonyLadyController.java
+++ SpringMVCExample/src/main/java/com/monyLady/myapp/controller/MonyLadyController.java
@@ -3,6 +3,7 @@ package com.monyLady.myapp.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -13,14 +14,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.monyLady.myapp.metiers.PersonManager;
 import com.monyLady.myapp.model.Person;
+import com.monyLady.myapp.model.Product;
+import com.monyLady.myapp.model.Subcategory;
 
 
 @Controller
 
 public class MonyLadyController {
+	
+
 
 	
 	private PersonManager personManager;
+	
+	private static Logger logger = Logger.getLogger(MonyLadyController.class);
 	
 	@Autowired(required=true)
 	@Qualifier(value= "personManager")
@@ -61,7 +68,32 @@ public class MonyLadyController {
 	}
 	
 	
+  // Méthode pour l'ajout d'un produit	
+  @RequestMapping(value ="/addproduct", method= RequestMethod.POST)
+  public void addProduct(Product product){
+	  logger.info("Ajout d'un produit");
+  }
   
+  //Méthode pour lister tous les produits dans la table WishList
+  @RequestMapping(value ="/allProductWishList", method= RequestMethod.GET)
+  public List<Product> allProductWishList(){
+	  
+	  List <Product> allProductWishList = new ArrayList<Product>();
+	  logger.info("Liste de tous les produits dans la table WishList");
+	  
+	  return allProductWishList;
+  }
+  
+  //Méthode pour lister tous les produit d'un sous catégorie
+  @RequestMapping(value="/allProduct", method = RequestMethod.GET)
+  public List<Product> allProduct (Subcategory subcategory){
+	  
+	  logger.info("liste de tous les produits du sous categorie :" + subcategory);;
+	  List<Product> allProduct = new  ArrayList<Product>();
+	  
+	  return allProduct;
+  }
+   
      
    
    

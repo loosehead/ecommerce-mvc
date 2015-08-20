496b01a811117b043c3f1c3fa2ac51fb597c3f82
aouldch <aboubecrine.ould-cheikhna@soprasteria.com>
2015-08-20 18:31:22 +0200

    Mise en place du détail produit

--- SpringMVCExample/src/main/webapp/js/AjaxLoading.js
+++ SpringMVCExample/src/main/webapp/js/AjaxLoading.js
@@ -6,20 +6,18 @@ var personne = {
 	password : ""
 };
 // localStorage.loginPerson='{}';
-//localStorage.pannier =  [];
+// localStorage.pannier = [];
 
-//var panierStored= localStorage["pannier"];
-//if(panierStored == null || panierStored ==""){
+// var panierStored= localStorage["pannier"];
+// if(panierStored == null || panierStored ==""){
 //
-//	localStorage.removeItem("pannier");
-//	var tempPannier = [];
-//	localStorage["pannier"] =  tempPannier;
-//}
+// localStorage.removeItem("pannier");
+// var tempPannier = [];
+// localStorage["pannier"] = tempPannier;
+// }
 
-	
+function tests() {
 
-function tests(){
-	
 }
 function AjaxCaller(action, data, function_success, function_error) {
 
@@ -87,33 +85,111 @@ function Ajax_creatUser() {
 
 }
 
-function loadByCategory(category, subcategory){
-	var data = {"categoryName": category, "subcategoryName":subcategory};
+function loadByCategory(category, subcategory) {
+	var data = {
+		"categoryName" : category,
+		"subcategoryName" : subcategory
+	};
 	localStorage.setItem("_borneInflistProduits", 0);
 	localStorage.setItem("_borneSuplistProduits", 0);
-	AjaxCaller("allProductBySubcategoryName", data, success_loadProduct, error_loadProduct);
+	AjaxCaller("allProductBySubcategoryName", data, success_loadProduct,
+			error_loadProduct);
 }
 
 function maj_pannier(produitName) {
 	var storedproduits = JSON.parse(localStorage["tempProduit"]);
-	for (i = 0, len =storedproduits.length; i < len; i++) {
-		if(storedproduits[i].productName == produitName){
-			//var existingEntry = localStorage["pannier"];
-			
-		    if(localStorage.getItem("pannier") == null || localStorage.getItem("pannier") =="" ) {
-		    	var noTexistingEntries = [];
-		    	noTexistingEntries.push(storedproduits[i]);
-		    	localStorage.setItem("pannier", JSON.stringify(noTexistingEntries));
-		    } else{
-		    	var existingEntries = JSON.parse(localStorage.getItem("pannier"));
-		    	existingEntries.push(storedproduits[i]);
-			    localStorage.setItem("pannier", JSON.stringify(existingEntries));		    	
-		    }
-		    
-		    
+	for (i = 0, len = storedproduits.length; i < len; i++) {
+		if (storedproduits[i].productName == produitName) {
+			// var existingEntry = localStorage["pannier"];
+
+			if (localStorage.getItem("pannier") == null
+					|| localStorage.getItem("pannier") == "") {
+				var noTexistingEntries = [];
+				noTexistingEntries.push(storedproduits[i]);
+				localStorage.setItem("pannier", JSON
+						.stringify(noTexistingEntries));
+			} else {
+				var existingEntries = JSON.parse(localStorage
+						.getItem("pannier"));
+				existingEntries.push(storedproduits[i]);
+				localStorage
+						.setItem("pannier", JSON.stringify(existingEntries));
+			}
+
+		}
+
+	}
+}
+
+function chargerDetails(produitName) {
+	var storedproduits = JSON.parse(localStorage["tempProduit"]);
+	var htmlDetails = "";
+	for (i = 0, len = storedproduits.length; i < len; i++) {
+		if (storedproduits[i].productName == produitName) {
+			htmlDetails = htmlDetails
+					+ "<div class='col-sm-5'>"
+					+ "<div class='view-product'>"
+					+ " <img src='images/tempHayat/bigImages/"
+					+ storedproduits[i].imageName
+					+ "' alt='' />"
+					+ "<h3>ZOOM</h3>"
+					+ "</div>"
+					+ "<div id='similar-product' class='carousel slide' data-ride='carousel'>"
+					+ " <!-- Wrapper for slides -->"
+					+ " <div class='carousel-inner'>"
+					+ "<div class='item active'>"
+					+ "<a href=''><img src='images/product-details/similar1.jpg' alt='></a>"
+					+ "<a href=''><img src='images/product-details/similar2.jpg' alt=''></a>"
+					+ "<a href=''><img src='images/product-details/similar3.jpg' alt=''></a>"
+					+ "</div>"
+					+ "<div class='item'>"
+					+ "<a href=''><img src='images/product-details/similar1.jpg' alt=''></a>"
+					+ "<a href=''><img src='images/product-details/similar2.jpg' alt=''></a>"
+					+ "<a href=''><img src='images/product-details/similar3.jpg' alt=''></a>"
+					+ "</div>"
+					+ "<div class='item'>"
+					+ "<a href=''><img src='images/product-details/similar1.jpg' alt=''></a>"
+					+ "<a href=''><img src='images/product-details/similar2.jpg' alt=''></a>"
+					+ "<a href=''><img src='images/product-details/similar3.jpg' alt=''></a>"
+					+ "</div>"
+					+ "</div>"
+					+ " <!-- Controls -->"
+					+ " <a class='left item-control' href='#similar-product' data-slide='prev'>"
+					+ "<i class='fa fa-angle-left'></i>"
+					+ " </a>"
+					+ " <a class='right item-control' href='#similar-product' data-slide='next'>"
+					+ "<i class='fa fa-angle-right'></i>"
+					+ "</a>"
+					+ "</div>"
+					+ "</div>"
+					+ "<div class='col-sm-7'>"
+					+ "<div class='product-information'><!--/product-information-->"
+					+ "<img src='images/product-details/new.jpg' class='newarrival' alt='' />"
+					+ "<h2>Anne Klein Sleeveless Colorblock Scuba</h2>"
+					+ "<p>Web ID: 1089772</p>"
+					+ "<img src='images/product-details/rating.png' alt='' />"
+					+ "<span>"
+					+ "<span>"+ storedproduits[i].price
+					+ "€</span>"
+					+ "<label>Quantity:</label>"
+					+ "<input type='text' value='3' />"
+					+ "<button type='button' class='btn btn-fefault cart'>"
+					+ "<i class='fa fa-shopping-cart'></i>"
+					+ "Add to cart"
+					+ "</button>"
+					+ "</span>"
+					+ "<p><b>Availability:</b> In Stock</p>"
+					+ "<p><b>Condition:</b> New</p>"
+					+ "<p><b>Brand:</b> E-SHOPPER</p>"
+					+ "<a href=''><img src='images/product-details/share.png' class='share img-responsive'  alt='' /></a>"
+					+ "</div><!--/product-information-->" + "</div>"
+					+ "</div><!--/product-details-->";
+			break;
 		}
 		
+
 	}
+	$("#prodItems").html(htmlDetails);
 }
 
 function Ajax_loginUser() {
@@ -131,26 +207,30 @@ function Ajax_loginUser() {
 
 function success_loadProduct(listProducts) {
 
-	if(parseInt(localStorage.getItem("_borneSuplistProduits"))+6 >  listProducts.length ){
-		localStorage.setItem("_borneSuplistProduits",parseInt(localStorage.getItem("_borneSuplistProduits"))+(listProducts.length - parseInt(localStorage.getItem("_borneSuplistProduits"))));
-	}else{
-		localStorage.setItem("_borneSuplistProduits",parseInt(localStorage.getItem("_borneSuplistProduits"))+6);
+	if (parseInt(localStorage.getItem("_borneSuplistProduits")) + 6 > listProducts.length) {
+		localStorage.setItem("_borneSuplistProduits", parseInt(localStorage
+				.getItem("_borneSuplistProduits"))
+				+ (listProducts.length - parseInt(localStorage
+						.getItem("_borneSuplistProduits"))));
+	} else {
+		localStorage.setItem("_borneSuplistProduits", parseInt(localStorage
+				.getItem("_borneSuplistProduits")) + 6);
 	}
 	var taille = 0;
-	if(parseInt(localStorage.getItem("_borneSuplistProduits")) == listProducts.length){
+	if (parseInt(localStorage.getItem("_borneSuplistProduits")) == listProducts.length) {
 		taille = listProducts.length;
-		$( ".next_prod" ).html("revenir au debut");
-		
+		$(".next_prod").html("revenir au debut");
+
 		localStorage.setItem("_borneSuplistProduits", 0);
-	}else{
-		taille = parseInt(localStorage.getItem("_borneSuplistProduits")) % listProducts.length;
+	} else {
+		taille = parseInt(localStorage.getItem("_borneSuplistProduits"))
+				% listProducts.length;
 	}
-	
-	
-	if(parseInt(localStorage.getItem("_borneInflistProduits")) > taille){
-		localStorage.setItem("_borneInflistProduits",0);
+
+	if (parseInt(localStorage.getItem("_borneInflistProduits")) > taille) {
+		localStorage.setItem("_borneInflistProduits", 0);
 	}
-	//localStorage.tempProduit = listProducts; 
+	// localStorage.tempProduit = listProducts;
 	var borneInf = parseInt(localStorage.getItem("_borneInflistProduits"));
 	localStorage["tempProduit"] = JSON.stringify(listProducts);
 	var _htmlProductList = '<h2 class="title text-center">Features Items</h2>';
@@ -183,42 +263,49 @@ function success_loadProduct(listProducts) {
 				+ "<li><a href='javascript:maj_pannier(\""
 				+ p.productName
 				+ "\")'><i class='fa fa-plus-square'></i>Add to wishlist</a></li>"
-				+ "<li><a href='#'><i class='fa fa-plus-square'></i>Détails</a></li>"
+				+ "<li><a href='javascript:chargerDetails(\"" + p.productName
+				+ "\")'><i class='fa fa-plus-square'></i>Détails</a></li>"
 				+ "</ul></div></div>" + "</div>";
 
 	}
 
-	localStorage.setItem("_borneInflistProduits",taille);
+	localStorage.setItem("_borneInflistProduits", taille);
 	$("#prodItems").html(_htmlProductList);
 
 }
 
-function showNextProducts(){
-	$( ".prev_prod" ).show();
+function showNextProducts() {
+	$(".prev_prod").show();
 	success_loadProduct(JSON.parse(localStorage.getItem("tempProduit")));
 }
 
-function showPrevProducts(){
-	$( ".next_prod" ).show();
+function showPrevProducts() {
+	$(".next_prod").show();
 	localStorage.setItem("_borneInflistProduits", 0);
 	localStorage.setItem("_borneSuplistProduits", 0);
 	success_loadProduct(JSON.parse(localStorage.getItem("tempProduit")));
 }
 function updatePannier(contentPan) {
-	var _htmlProductListPan ="";
+	var _htmlProductListPan = "";
 	for (i = 0, len = contentPan.length; i < len; i++) {
 		var p = contentPan[i];
 		_htmlProductListPan = _htmlProductListPan
-		+"<tr>"
+				+ "<tr>"
 				+ "<td class='cart_product'>"
-				+ "<a href=''><img src='images/tempHayat/smallImages/"+p.imageName+"' alt=''></a>"
+				+ "<a href=''><img src='images/tempHayat/smallImages/"
+				+ p.imageName
+				+ "' alt=''></a>"
 				+ "</td>"
 				+ "<td class='cart_description'>"
 				+ "<h4><a href=''>Colorblock Scuba</a></h4>"
-				+ "<p>"+p.description+"</p>"
+				+ "<p>"
+				+ p.description
+				+ "</p>"
 				+ "</td>"
 				+ "<td class='cart_price'>"
-				+ "<p>"+p.price+"</p>"
+				+ "<p>"
+				+ p.price
+				+ "</p>"
 				+ "</td>"
 				+ "<td class='cart_quantity'>"
 				+ "<div class='cart_quantity_button'>"
@@ -228,7 +315,9 @@ function updatePannier(contentPan) {
 				+ "</div>"
 				+ "</td>"
 				+ "<td class='cart_total'>"
-				+ "<p class='cart_total_price'>"+p.price+"</p>"
+				+ "<p class='cart_total_price'>"
+				+ p.price
+				+ "</p>"
 				+ "</td>"
 				+ "<td class='cart_delete'>"
 				+ "<a class='cart_quantity_delete' href=''><i class='fa fa-times'></i></a>"
@@ -236,8 +325,3 @@ function updatePannier(contentPan) {
 	}
 	$("#listProdBuy").html(_htmlProductListPan);
 }
-
-
-
-
-
